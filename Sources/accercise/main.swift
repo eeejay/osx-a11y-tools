a2fd652bfb95248c68fef5e5e7bc51cab37556e7
Eitan Isaacson <eitan@monotonous.org>
2020-02-21 14:36:27 -0800

    added many more attributes. support array attributes

--- Sources/accercise/main.swift
+++ Sources/accercise/main.swift
@@ -3,49 +3,88 @@ import Cocoa
 import CoreFoundation
 import Commander
 
-let basic = [kAXRoleAttribute, kAXTitleAttribute, kAXDescriptionAttribute, kAXValueAttribute]
-let additional = [kAXSubroleAttribute, kAXPositionAttribute, kAXSizeAttribute]
+let basic = ["AXRole", "AXTitle", "AXDescription", "AXValue"]
+let additional = [ "AXSelectedCells", "AXVisibleCells", "AXRowIndexRange",
+    "AXColumnIndexRange", "AXCellForColumnAndRow", "AXBlockQuoteLevel",
+    "AXAccessKey", "AXValueAutofilled", "AXValueAutofillAvailable",
+    "AXValueAutofillType", "AXLanguage", "AXRequired", "AXInvalid", "AXGrabbed",
+    "AXDateTimeValue", "AXInlineText", "AXDropEffects", "AXARIALive",
+    "AXARIAAtomic", "AXARIARelevant", "AXElementBusy", "AXARIAPosInSet",
+    "AXARIASetSize", "AXLoadingProgress", "AXHasPopup", "AXPopupValue",
+    "AXPlaceholderValue", "AXTextMarkerIsValid", "AXIndexForTextMarker",
+    "AXTextMarkerForIndex", "AXPath", "AXExpandedTextValue",
+    "AXIsMultiSelectable", "AXDocumentURI", "AXDocumentEncoding",
+    "AXARIAControls", "AXDOMIdentifier", "AXDOMClassList", "AXARIACurrent",
+    "AXARIAColumnIndex", "AXARIARowIndex", "AXARIAColumnCount", "AXARIARowCount",
+    "AXUIElementCountForSearchPredicate", "AXUIElementsForSearchPredicate",
+    "AXEndTextMarkerForBounds", "AXStartTextMarkerForBounds",
+    "AXLineTextMarkerRangeForTextMarker", "AXMisspellingTextMarkerRange",
+    "AXSelectTextWithCriteria", "AXSearchTextWithCriteria", "AXTextOperation",
+    "AXPreventKeyboardDOMEventDispatch", "AXCaretBrowsingEnabled",
+    "AXWebSessionID", "AXFocusableAncestor", "AXEditableAncestor",
+    "AXHighestEditableAncestor", "AXLinkRelationshipType", "AXRelativeFrame"]
 
+// XXX: Let's use this later
+let math = ["AXMathRootRadicand", "AXMathRootIndex", "AXMathFractionDenominator",
+    "AXMathFractionNumerator", "AXMathBase", "AXMathSubscript",
+    "AXMathSuperscript", "AXMathUnder", "AXMathOver", "AXMathFencedOpen",
+    "AXMathFencedClose", "AXMathLineThickness", "AXMathPrescripts",
+    "AXMathPostscripts"]
+
+// XXX: AXChildrenInNavigationOrder
+// XXX: AXOwns - we do this in our internal a11y tree.
+
+let extents = ["AXPosition", "AXSize"]
 func getAttribute(_ element: AXUIElement, _ name: String) -> AnyObject? {
     var value: AnyObject?
     AXUIElementCopyAttributeValue(element, name as CFString, &value)
     return value
 }
 
+func anyObjectToString(_ obj: AnyObject) -> String {
+    if (obj is String) {
+        return obj as? String ?? ""
+    } else if (obj is NSValue) {
+        return String(format: "%d", obj as? Int ?? 0)
+    } else if (obj is NSArray) {
+        var strArray: [String] = []
+        for elm in (obj as? [AnyObject] ?? []) {
+            strArray.append(anyObjectToString(elm))
+        }
+        return strArray.joined(separator:",")
+    } else {
+        let value = obj as! AXValue
+        let type = AXValueGetType(value);
+        switch type {
+        case .axError:
+            var res: AXError = .success;
+            AXValueGetValue(value, type, &res);
+            return "\(res)"
+        case .cfRange:
+            var res: CFRange = CFRange();
+            AXValueGetValue(value, type, &res);
+            return "\(res)"
+        case .cgPoint:
+            var res: CGPoint = CGPoint.zero;
+            AXValueGetValue(value, type, &res);
+            return "\(res)"
+        case .cgRect:
+            var res: CGRect = CGRect.zero;
+            AXValueGetValue(value, type, &res);
+            return "\(res)"
+        case .cgSize:
+            var res: CGSize = CGSize.zero;
+            AXValueGetValue(value, type, &res);
+            return "\(res)"
+        default:
+            return "\(value) \(type)"
+        }
+    }
+}
+
 func getAttributeAsString(_ element: AXUIElement, _ name: String) -> String {
     if let val = getAttribute(element, name) {
-        if (val is String) {
-            return val as? String ?? ""
-        } else if (val is NSValue) {
-            return String(format: "%d", val as? Int ?? 0)
-        } else {
-            let value = val as! AXValue
-            let type = AXValueGetType(value);
-            switch type {
-            case .axError:
-                var res: AXError = .success;
-                AXValueGetValue(value, type, &res);
-                return "\(res)"
-            case .cfRange:
-                var res: CFRange = CFRange();
-                AXValueGetValue(value, type, &res);
-                return "\(res)"
-            case .cgPoint:
-                var res: CGPoint = CGPoint.zero;
-                AXValueGetValue(value, type, &res);
-                return "\(res)"
-            case .cgRect:
-                var res: CGRect = CGRect.zero;
-                AXValueGetValue(value, type, &res);
-                return "\(res)"
-            case .cgSize:
-                var res: CGSize = CGSize.zero;
-                AXValueGetValue(value, type, &res);
-                return "\(res)"
-            default:
-                return "\(value)"
-            }            
-        }
+        return anyObjectToString(val)
     }
 
     return ""
@@ -53,7 +92,7 @@ func getAttributeAsString(_ element: AXUIElement, _ name: String) -> String {
 
 func dumpTree(_ element: AXUIElement, _ attributes: [String], _ indent: Int = 0) {
 	let prefixIndent = String(repeating: " ", count: indent)
-    let roleDesc = getAttribute(element, kAXRoleDescriptionAttribute) as? String ?? "unknown"
+    let roleDesc = getAttribute(element, "AXRoleDescription") as? String ?? "unknown"
     var details = [String]()
     for attr in attributes {
         let val = getAttributeAsString(element, attr)
@@ -61,7 +100,7 @@ func dumpTree(_ element: AXUIElement, _ attributes: [String], _ indent: Int = 0)
     }
     let detailsStr = details.joined(separator: " ")
     print("\(prefixIndent)\(roleDesc) : \(detailsStr)")
-    let children = getAttribute(element, kAXChildrenAttribute) as? [AXUIElement] ?? []
+    let children = getAttribute(element, "AXChildren") as? [AXUIElement] ?? []
     for child in children {
     	dumpTree(child, attributes, indent + 1)
     }
@@ -78,7 +117,7 @@ func getRootElement(pid: Int, name: String) -> AXUIElement? {
                 }
             } else {
                 let appRef = AXUIElementCreateApplication(p)
-                if (name == getAttribute(appRef, kAXTitleAttribute) as? String ?? "") {
+                if (name == getAttribute(appRef, "AXTitle") as? String ?? "") {
                     return appRef
                 }
             }
@@ -89,11 +128,11 @@ func getRootElement(pid: Int, name: String) -> AXUIElement? {
 }
 
 func findWebArea(_ element: AXUIElement) -> AXUIElement? {
-    let role = getAttribute(element, kAXRoleAttribute) as? String ?? ""
+    let role = getAttribute(element, "AXRole") as? String ?? ""
     if (role == "AXWebArea") {
         return element;
     }
-    let children = getAttribute(element, kAXChildrenAttribute) as? [AXUIElement] ?? []
+    let children = getAttribute(element, "AXChildren") as? [AXUIElement] ?? []
     for child in children {
         if let webArea = findWebArea(child) {
             return webArea
@@ -104,8 +143,7 @@ func findWebArea(_ element: AXUIElement) -> AXUIElement? {
 }
 
 let main = Group {
-  $0.command("tree",
-    Option("pid", default: 0, description: "Target app PID"),
+  $0.command("tree",    Option("pid", default: 0, description: "Target app PID"),
     Option("app", default: "Nightly", description: "Target app name. Ignored if PID is provided"),
     Flag("web", description: "Only output web area subtree"),
     Flag("extras", description: "Show additional attributes"),
